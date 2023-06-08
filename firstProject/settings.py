e4319d72140d9452775b6476f0453b18f55c9f25
pdaDev <dapasynkov2001@gmail.com>
2023-06-08 18:00:20 +0700

    save results

--- firstProject/settings.py
+++ firstProject/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-%80a9+4cl(j4j$rw6$r&uszu&%%ffjr&_g=k(r-dzt__5+#$1f
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
@@ -122,7 +122,6 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
-ALLOWED_HOSTS = []
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
